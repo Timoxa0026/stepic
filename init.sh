1d0c8910cea5bc766e3a51d555ba2eb54174d669
Timoxa0026 <Denis_0026@inbox.ru>
2016-03-23 02:35:09 +0400

    1.9

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-
-sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default1
-
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/gunicorn.py hello:app
