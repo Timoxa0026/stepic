679eb62390a07526635a6ad7a329c97e3a7f6fdf
Timoxa0026 <Denis_0026@inbox.ru>
2016-03-23 01:55:52 +0400

    1.8

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default1
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
